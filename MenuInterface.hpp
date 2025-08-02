cd7d0ec7b0c05418544749474d294985fc5173e4
malak-emad <maloka.emad@gmail.com>
2025-08-02 06:48:06 +0300

    implementing an interface for all classes to use

--- MenuInterface.hpp
+++ MenuInterface.hpp
@@ -1,12 +1,24 @@
 #ifndef MENUINTERFACE_H
 #define MENUINTERFACE_H
+#include <memory> // for std::shared_ptr
+
+class User;  // Forward declaration
 
 class MenuInterface {
 public:
     virtual void showMenu() = 0;
     virtual void processChoice() = 0;
-    virtual ~MenuInterface() = default;
-};
 
+
+    // Add this:
+    virtual void setCurrentUser(std::shared_ptr<User> user) {
+        currentUser = user;
+    }
+
+    virtual ~MenuInterface() = default;
+
+protected:
+    std::shared_ptr<User> currentUser; // store the logged-in user
+};
 
 #endif
